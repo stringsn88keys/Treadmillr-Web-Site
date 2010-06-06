a586b990d0b908afea37a2c7c3ed012036b55f80
Thomas Powell <twilliampowell@gmail.com>
2010-06-06 07:57:27 -0400

    Cleanup

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <link rel="apple-touch-icon" href="apple-touch-icon.png" />
     <script type="text/javascript" src="http://www.google.com/jsapi"></script>
-    <script type="text/javascript"> google.load("jquery", "1.4"); </script>
+	 <script src="jquery-1.4.2.min.js" type="application/x-javascript" charset="utf-8"></script>
     <script src="jqtouch/jqtouch.min.js" type="application/x-javascript" charset="utf-8"></script>
+	 <script type="text/javascript" src="conversions.js"></script>
     <style type="text/css" media="screen">@import "jqtouch/jqtouch.min.css";</style>
     <style type="text/css" media="screen">@import "themes/jqt/theme.css";</style>
 
@@ -74,60 +75,6 @@ function fill_option_array($choices, $default)
             'themes/jqt/img/button_clicked.png'
             ]
     });
-
-function myinit()
-{
-	changepace();
-	changemph();
-	changeinterval();
-}
-
-function setTmill()
-{
-	element=document.getElementbyID("treadmill");
-	element.innerHTML="hey";
-}
-
-function changepace()
-{
-	var minutes=document.getElementsByName('minutes')[0].value;
-	var seconds=document.getElementsByName('seconds')[0].value;
-	var mph = 3600/(parseFloat(minutes)*60+parseFloat(seconds));
-
-
-	$('#speed').html('Speed:<br>'+mph.toFixed(2)+' MPH');
-}
-
-function changeinterval()
-{
-	var minutes=document.getElementsByName('intervalminutes')[0].value;
-	var seconds=document.getElementsByName('intervalseconds')[0].value;
-	var meters=document.getElementsByName('intervaldistance')[0].value;
-	var mph = 3600/(parseFloat(minutes)*60+parseFloat(seconds))*parseFloat(meters)/1609.344;
-
-
-	$('#intervalmph').html('Speed:<br>'+mph.toFixed(2)+' MPH');
-}
-
-function changemph()
-{
-	var mph=document.getElementsByName('mph')[0].value;
-	var seconds = 36000/parseFloat(mph);
-	var minutes = seconds / 60;
-	seconds = seconds % 60;
-	minutes = minutes.toFixed(0);
-	seconds = seconds.toFixed(1);
-	
-	if(seconds >= 10)
-	{
-		$('#pace').html('Pace:<br>'+minutes+':'+seconds);
-	}
-	else
-	{
-		$('#pace').html('Pace:<br>'+minutes+':0'+seconds);
-	}
-}
-
 </script>
 </head>
 <body onload="myinit()">
@@ -244,6 +191,9 @@ function changemph()
 				fill_option_array($distances, "10k");
 				echo '</select>';
 				?>
+			</li>
+			<li>
+				Under Construction
 			</li>
 		</ul>
 	</div>
